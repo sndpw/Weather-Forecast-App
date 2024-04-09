7633abb2d82bd5b6096cd1e902cc302aeb618aa4
sndpw <sandeepkumartirumala@gmail.com>
2024-04-10 01:23:29 +0530

    checkpoint 1

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,14 +3,14 @@ import axios, { AxiosResponse } from "axios";
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.RAPID_API_KEY || "",
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "ac253d845b2f0baa375baf3b55f8cb24";
+export const WEATHER_API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 
 export interface City {
   name: string;
